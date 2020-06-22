0cfd021592afb1fdf6c3d0fd296f5907c6d60deb
隼人 松崎 <toyotomi1588@gmail.com>
2020-06-22 15:37:39 +0900

    Heroのひな型を作りました

---
+++ Project1/Project1/ObjHero.cpp
@@ -0,0 +1,146 @@
+//使用するヘッダーファイル
+#include "GameL\DrawTexture.h"
+#include "GameL\WinInputs.h"
+#include "GameL\SceneManager.h"
+#include "GameL\HitBoxManager.h"
+
+#include "GameHead.h"
+#include "ObjHero.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjHero::Init()
+{
+	m_px = 70.0f;        //位置
+	m_py = 64.0f;
+	m_vx = 0.0f;        //移動ベクトル
+	m_vy = 0.0f;
+	m_posture = 1.0f;   //右向き0.0f　左向き1.0f
+
+	m_ani_time = 0;
+	m_ani_frame = 1;     //静止フレームを初期にする
+
+	m_speed_power = 0.5f;//通常速度
+	m_ani_max_time = 4;  //アニメーション間隔幅
+
+	//blockとの衝突状態確認用
+	bool m_hit_up = false;
+	bool m_hit_down = false;
+	bool m_hit_left = false;
+	bool m_hit_right = false;
+
+	m_block_type = 0;     //踏んでいるblockの種類を確認用
+
+	//当たり判定用のHitBoxを作成
+	Hits::SetHitBox(this, m_px, m_py, 64, 64, ELEMENT_PLAYER, OBJ_HERO, 1);
+}
+
+//アクション
+void CObjHero::Action()
+{
+	//移動ベクトルの破棄
+	m_vy = 0.0f;
+
+	//Xキー入力でジャンプ
+	if (Input::GetVKey('X') == true)
+	{
+		if (m_hit_down == true)
+		{
+			m_vy = -20;
+		}
+	}
+
+	
+
+
+	//キーの入力方向
+	if (Input::GetVKey(VK_RIGHT) == true)
+	{
+		m_vx += m_speed_power;
+		m_posture = 1.0f;
+		m_ani_time += 1;
+	}
+
+	else if (Input::GetVKey(VK_LEFT) == true)
+	{
+		m_vx -= m_speed_power;
+		m_posture = 0.0f;
+		m_ani_time += 1;
+	}
+	else
+	{
+		m_ani_frame = 1;  //キー入力が無い場合は静止フレームにする
+		m_ani_time = 0;
+	}
+
+	if (m_ani_time > m_ani_max_time)
+	{
+		m_ani_frame += 1;
+		m_ani_time = 0;
+	}
+
+	if (m_ani_frame == 4)
+	{
+		m_ani_frame = 0;
+	}
+
+	//摩擦
+	m_vx += -(m_vx * 0.098);
+
+	//自由落下運動
+	m_vy += 9.8 / (16.0f);	
+
+	//自身のHitBoxを持ってくる
+	CHitBox* hit = Hits::GetHitBox(this);
+	
+	//位置の更新
+	m_px += m_vx;
+	m_py += m_vy;
+
+	//HiitBoxの位置の変更
+	hit->SetPos(m_px, m_py);
+}
+
+//ドロー
+void CObjHero::Draw()
+{
+	int AniData[4] =
+	{
+		1,0,2,0,
+	};
+
+	//描画カラー情報　
+	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
+
+	RECT_F src; //描画元切り取り位置
+	RECT_F dst; //描画先表示位置
+
+	//切り取り位置の設定
+	src.m_top = 0.0f;
+	src.m_left = 0.0f + AniData[m_ani_frame] * 64;
+	src.m_right = 64.0f + AniData[m_ani_frame] * 64;
+	src.m_bottom = 64.0f;
+
+	//表示位置の設定
+	dst.m_top = 0.0f + m_py;
+	dst.m_left = (64.0f * m_posture) + m_px;
+	dst.m_right = (64 - 64.0f * m_posture) + m_px;
+	dst.m_bottom = 64.0f + m_py;
+
+	//描画
+	Draw::Draw(0, &src, &dst, c, 0.0f);
+
+	//交点
+	float cc[4] = { 1.0f,0.0f,0.0f,1.0f };
+	src.m_top = 0.0f;
+	src.m_left = 320.0f;
+	src.m_right = 320.0f + 64.0f;
+	src.m_bottom = 64.0f;
+	dst.m_top = py;
+	dst.m_left = px;
+	dst.m_right = dst.m_left + 10.0f;
+	dst.m_bottom = dst.m_top + 10.0f;
+	Draw::Draw(0, &src, &dst, cc, 0.0f);
+}
